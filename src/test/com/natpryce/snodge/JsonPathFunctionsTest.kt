b844cb8035d35a7e803ace886890850e2aa68597
Nat Pryce <sw@natpryce.com>
2016-08-08 11:03:00 +0530

    Converting tests to Kotlin

---
+++ src/test/com/natpryce/snodge/JsonPathFunctionsTest.kt
@@ -0,0 +1,66 @@
+package com.natpryce.snodge
+
+import org.junit.Test
+
+import com.natpryce.snodge.JsonPath.functions.endsWith
+import com.natpryce.snodge.JsonPath.functions.startsWith
+import org.junit.Assert.assertTrue
+import java.util.function.Predicate
+
+class JsonPathFunctionsTest {
+    @Test
+    fun pathEndsWith() {
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), endsWith("a", "b", "c"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), endsWith("b", "c"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), endsWith("c"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), endsWith())
+        
+        assertPathMeetsPredicate(JsonPath.of(), endsWith())
+        
+        assertPathMeetsPredicate(JsonPath.of("a", "b", 1), endsWith(1))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", 1), endsWith("b", 1))
+        
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(endsWith("x", "a", "b", "c")))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(endsWith("a", "b", "c", "x")))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(endsWith("x")))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", 1), not(endsWith("x")))
+        
+        assertPathMeetsPredicate(JsonPath.of(), not(endsWith("x")))
+        
+        // Different to JavaScript
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "1"), not(endsWith(1)))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", 1), not(endsWith("1")))
+    }
+    
+    @Test
+    fun pathStartsWith() {
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), startsWith("a", "b", "c"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), startsWith("a", "b"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), startsWith("a"))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), startsWith())
+        
+        assertPathMeetsPredicate(JsonPath.of(), startsWith())
+        
+        assertPathMeetsPredicate(JsonPath.of(1, "a", "b"), startsWith(1))
+        assertPathMeetsPredicate(JsonPath.of(1, "a", "b"), startsWith(1, "a"))
+        
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(startsWith("x", "a", "b", "c")))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(startsWith("a", "b", "c", "x")))
+        assertPathMeetsPredicate(JsonPath.of("a", "b", "c"), not(startsWith("x")))
+        assertPathMeetsPredicate(JsonPath.of(1, "a", "b"), not(startsWith("x")))
+        
+        assertPathMeetsPredicate(JsonPath.of(), not(startsWith("x")))
+        
+        // Different to JavaScript
+        assertPathMeetsPredicate(JsonPath.of("1", "a", "b"), not(startsWith(1)))
+        assertPathMeetsPredicate(JsonPath.of(1, "a", "b"), not(startsWith("1")))
+    }
+    
+    private fun not(p: Predicate<JsonPath>): Predicate<JsonPath> {
+        return p.negate()
+    }
+    
+    private fun assertPathMeetsPredicate(path: JsonPath, p: Predicate<JsonPath>) {
+        assertTrue("$path should meet predicate $p", p.test(path))
+    }
+}
