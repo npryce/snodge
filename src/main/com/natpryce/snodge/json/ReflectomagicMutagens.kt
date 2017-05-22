13be32c2abe66f0ba5c31ed3eb66012900915eac
Nat Pryce <sw@natpryce.com>
2017-05-22 08:13:20 +0100

    Separated the troublesome classes for reflection from the JSON wrangling

--- src/main/com/natpryce/snodge/json/ReflectomagicMutagens.kt
+++ src/main/com/natpryce/snodge/json/ReflectomagicMutagens.kt
@@ -1,35 +1,9 @@
 package com.natpryce.snodge.json
 
 import com.google.gson.JsonPrimitive
-import kotlin.reflect.jvm.jvmName
-
-class CodeExecutionVulnerability(message: String): Error(message)
-
-class ClassCannotBeLoaded {
-    companion object {
-        init {
-            throw CodeExecutionVulnerability("tried to load an arbitrary class")
-        }
-    }
-}
-
-class ClassCannotBeInstantiated {
-    init {
-        throw CodeExecutionVulnerability("tried to instantiate an arbitrary class")
-    }
-}
-
-private class PrivateClass {
-}
-
+import com.natpryce.snodge.reflect.troublesomeClasses
 
 fun securityMutagens() =
-    listOf(
-        ClassCannotBeLoaded::class,
-        ClassCannotBeInstantiated::class,
-        PrivateClass::class
-    ).map { c ->
-        replaceJsonElement(JsonPrimitive(c.jvmName))
-    }.let {
-        combine(it)
-    }
+    troublesomeClasses()
+        .map { replaceJsonElement(JsonPrimitive(it)).ifElement { it is JsonPrimitive && it.isString} }
+        .let { combine(it) }
---
+++ src/main/com/natpryce/snodge/reflect/TroublesomeClasses.kt
@@ -0,0 +1,26 @@
+package com.natpryce.snodge.reflect
+
+import kotlin.reflect.jvm.jvmName
+
+class CodeExecutionVulnerability(message: String) : Error(message)
+
+class ClassCannotBeLoaded {
+    companion object {
+        init {
+            throw CodeExecutionVulnerability("tried to load an arbitrary class")
+        }
+    }
+}
+
+class ClassCannotBeInstantiated {
+    init {
+        throw CodeExecutionVulnerability("tried to instantiate an arbitrary class")
+    }
+}
+
+private class PrivateClass {
+}
+
+fun troublesomeClasses() =
+    listOf(ClassCannotBeLoaded::class, ClassCannotBeInstantiated::class, PrivateClass::class)
+        .map { it.jvmName }
