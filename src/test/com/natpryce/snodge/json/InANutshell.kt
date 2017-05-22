79ced5a986aa005a88278f27bc2ddf90adc5616d
Nat Pryce <sw@natpryce.com>
2017-05-22 07:37:31 +0100

    Kotlinified the example

--- src/test/com/natpryce/snodge/json/InANutshell.kt
+++ src/test/com/natpryce/snodge/json/InANutshell.kt
@@ -3,12 +3,10 @@ package com.natpryce.snodge.json
 import com.natpryce.snodge.mutants
 import java.util.Random
 
-object InANutshell {
-    @JvmStatic fun main(args: Array<String>) {
-        val random = Random()
-        val originalJson = "{\"x\": \"hello\", \"y\": [1,2,3]}"
-        
-        random.mutants(allJsonMutagens().forStrings(), 10, originalJson)
-            .forEach(::println)
-    }
+fun main(args: Array<String>) {
+    val random = Random()
+    val originalJson = "{\"x\": \"hello\", \"y\": [1,2,3]}"
+    
+    random.mutants(allJsonMutagens().forStrings(), 10, originalJson)
+        .forEach(::println)
 }
