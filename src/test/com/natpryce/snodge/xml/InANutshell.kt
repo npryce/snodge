fa9232392266b456bc73c0382d500ac8b247b093
Nat Pryce <sw@natpryce.com>
2017-06-09 14:39:51 +0100

    Example XML mutation

---
+++ src/test/com/natpryce/snodge/xml/InANutshell.kt
@@ -0,0 +1,17 @@
+package com.natpryce.snodge.xml
+
+import com.natpryce.snodge.mutants
+import com.natpryce.xmlk.ExampleXmlFiles
+import com.natpryce.xmlk.toXmlString
+import java.util.Random
+
+fun main(args: Array<String>) {
+    val random = Random()
+    val originalXml = ExampleXmlFiles.load("simple-example.xml")
+    
+    random.mutants(allXmlMutagens(), 10, originalXml)
+        .forEach {
+            println(it.toXmlString())
+            println()
+        }
+}
