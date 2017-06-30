e81d124c8b735860b5c3d314100baf850aca9644
Nat Pryce <sw@natpryce.com>
2017-06-30 19:39:51 +0100

    Split out cross-platform JSON mutagen tests from JVM-specific tests

---
+++ platform/jvm/src/test/com/natpryce/snodge/json/JsonMutagenForEncodedStringsTest.kt
@@ -0,0 +1,40 @@
+package com.natpryce.snodge.json
+
+import com.natpryce.jsonk.JsonNull
+import com.natpryce.jsonk.toJsonElement
+import com.natpryce.jsonk.toJsonString
+import com.natpryce.snodge.Random
+import com.natpryce.snodge.mutant
+import com.natpryce.snodge.plus
+import org.junit.Test
+import java.nio.charset.Charset
+import kotlin.test.assertNotEquals
+
+class JsonMutagenForEncodedStringsTest {
+    val random = Random()
+    val mutagen = addObjectProperty(JsonNull) + addArrayElement(JsonNull)
+    
+    @Test
+    fun can_mutate_encoded_json_text() {
+        val charset = Charset.forName("UTF-8")
+        
+        val originalString = obj(
+            "num" to 1,
+            "list" to list(1, 2, 3)).toJsonString()
+        
+        val originalBytes = originalString.toByteArray(charset)
+        
+        val mutatedBytes = random.mutant(mutagen.forEncodedStrings(charset), originalBytes)
+        
+        assertNotEquals(actual = mutatedBytes, illegal = originalBytes)
+        
+        val mutatedString = String(mutatedBytes, charset)
+        
+        canParse(mutatedString)
+    }
+    
+    private fun canParse(mutated: String) {
+        // Does not blow up
+        mutated.toJsonElement()
+    }
+}
---
+++ platform/jvm/src/test/com/natpryce/snodge/json/JsonMutagenForStringsTest.kt
@@ -0,0 +1,35 @@
+package com.natpryce.snodge.json
+
+import com.natpryce.jsonk.JsonNull
+import com.natpryce.jsonk.toJsonElement
+import com.natpryce.snodge.Random
+import com.natpryce.snodge.mutant
+import com.natpryce.snodge.plus
+import org.junit.Test
+import kotlin.test.assertNotEquals
+
+class JsonMutagenForStringsTest {
+    val mutagen = addObjectProperty(JsonNull) + addArrayElement(JsonNull)
+    val random = Random()
+    
+    @Test
+    fun can_mutate_json_text() {
+        val original = """
+        {
+            "num": 1,
+            "list": [1,2,3]
+        }
+        """
+        
+        val mutant = random.mutant(mutagen.forStrings(), original)
+        
+        assertNotEquals(actual = mutant, illegal = original)
+        
+        canParse(mutant)
+    }
+    
+    private fun canParse(mutated: String) {
+        // Does not blow up
+        mutated.toJsonElement()
+    }
+}
