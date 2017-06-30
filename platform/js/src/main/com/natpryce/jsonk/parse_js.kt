17678de0be97b3558deb1ff4d6cca06ac26bc849
Nat Pryce <sw@natpryce.com>
2017-06-30 22:45:02 +0100

    Mutagen<JsonNode>.forStrings() implemented for JS platform

--- platform/js/src/main/com/natpryce/jsonk/parse_js.kt
+++ platform/js/src/main/com/natpryce/jsonk/parse_js.kt
@@ -1,5 +1,29 @@
 package com.natpryce.jsonk
 
-fun String.toJsonElement(): JsonElement {
-    TODO("not implemented")
+import kotlin.js.Json
+
+
+fun String.toJsonElement(): JsonElement =
+    JSON.parse<Any?>(this).toJsonElement()
+
+private fun Any?.toJsonElement(): JsonElement {
+    return when(this) {
+        undefined, null -> JsonNull
+        is Number -> JsonNumber(toString())
+        is String -> JsonString(this)
+        is Boolean -> JsonBoolean(this)
+        is Array<*> -> JsonArray(this.map { it.toJsonElement() })
+        else -> { // assume object
+            @Suppress("UNCHECKED_CAST_TO_NATIVE_INTERFACE")
+            val json: Json = this as Json
+            JsonObject(Object.keys(json).map { it to json[it].toJsonElement() })
+        }
+    }
 }
+
+external private class Object {
+    companion object {
+        fun keys(o: Any?): Array<out String>
+    }
+}
+
--- platform/js/src/main/com/natpryce/jsonk/write_js.kt
+++ platform/js/src/main/com/natpryce/jsonk/write_js.kt
@@ -1,5 +1,18 @@
 package com.natpryce.jsonk
 
+import kotlin.js.json
+
 fun JsonElement.toJsonString(): String {
-    TODO("not implemented")
+    return JSON.stringify(this.toJavaScriptObject())
+}
+
+private fun JsonElement.toJavaScriptObject(): Any? {
+    return when(this) {
+        is JsonObject -> json(*this.map { it.key to it.value.toJavaScriptObject() }.toTypedArray())
+        is JsonArray -> this.map { it.toJavaScriptObject() }
+        is JsonString -> this.value
+        is JsonBoolean -> this.value
+        is JsonNull -> null
+        is JsonNumber -> this.toDouble()
+    }
 }
