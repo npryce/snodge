f51918a353b3906bd83acca0d260cbbb62ee886f
Nat Pryce <sw@natpryce.com>
2017-06-30 11:51:46 +0100

    Move tests of ByteArray mutagens to the common source tree

--- common/src/test/com/natpryce/snodge/bytes/ByteArrayReplaceRangeTest.kt
+++ common/src/test/com/natpryce/snodge/bytes/ByteArrayReplaceRangeTest.kt
@@ -1,33 +1,32 @@
 package com.natpryce.snodge.bytes
 
-import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.equalTo
 import com.natpryce.snodge.internal.replaceRange
 import org.junit.Test
+import kotlin.test.assertEquals
 
 
 class ByteArrayReplaceRangeTest {
     @Test
-    fun `replaces a range of bytes within array`() {
+    fun replaces_a_range_of_bytes_within_array() {
         val original = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
         val spliced = original.replaceRange(2..5, byteArrayOf(10, 11, 12))
         
-        assertThat(spliced.asList(), equalTo(byteArrayOf(1, 2, 10, 11, 12, 7, 8).asList()))
+        assertEquals(byteArrayOf(1, 2, 10, 11, 12, 7, 8).asList(), spliced.asList())
     }
     
     @Test
-    fun `replaces a range of bytes at start of array`() {
+    fun replaces_a_range_of_bytes_at_start_of_array() {
         val original = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
         val spliced = original.replaceRange(0..3, byteArrayOf(10, 11, 12))
         
-        assertThat(spliced.asList(), equalTo(byteArrayOf(10, 11, 12, 5, 6, 7, 8).asList()))
+        assertEquals(byteArrayOf(10, 11, 12, 5, 6, 7, 8).asList(), spliced.asList())
     }
     
     @Test
-    fun `replaces a range of bytes at end of array`() {
+    fun replaces_a_range_of_bytes_at_end_of_array() {
         val original = byteArrayOf(1, 2, 3, 4, 5, 6, 7, 8)
         val spliced = original.replaceRange(4..7, byteArrayOf(10, 11, 12))
         
-        assertThat(spliced.asList(), equalTo(byteArrayOf(1, 2, 3, 4, 10, 11, 12).asList()))
+        assertEquals(byteArrayOf(1, 2, 3, 4, 10, 11, 12).asList(), spliced.asList())
     }
 }
--- common/src/test/com/natpryce/snodge/bytes/ByteArraySpliceTest.kt
+++ common/src/test/com/natpryce/snodge/bytes/ByteArraySpliceTest.kt
@@ -1,32 +1,30 @@
 package com.natpryce.snodge.bytes
 
-import com.natpryce.hamkrest.assertion.assertThat
-import com.natpryce.hamkrest.isEmpty
 import com.natpryce.snodge.Mutagen
 import com.natpryce.snodge.Random
 import com.natpryce.snodge.repeat
-import org.junit.Assert.assertTrue
 import org.junit.Test
+import kotlin.test.assertTrue
 
 class ByteArraySpliceTest {
     @Test
-    fun `splices bytes into array`() {
+    fun splices_bytes_into_array() {
         val mutagen = repeat(100, splice(byteArrayOf(1, 2)))
         val original = byteArrayOf(100, 101, 102, 103)
         
         val spliceLocations = mutableSetOf<Int>()
         
         mutagen(Random(), original).map { it.value }.forEach {
-            assertTrue("original: $original, mutant: $it", 1 in it && 2 in it)
+            assertTrue(1 in it && 2 in it, "original: $original, mutant: $it")
             
             spliceLocations += it.indexOf(1)
         }
         
-        assertThat("bytes should be spliced into array at different locations", spliceLocations, !isEmpty)
+        assertTrue(spliceLocations.size > 1, "bytes should be spliced into array at different locations")
     }
     
     @Test
-    fun `can mutate the replaced bytes`() {
+    fun can_mutate_the_replaced_bytes() {
         val sliceMutagen: Mutagen<ByteArray> =
             fun(_: Random, original: ByteArray) =
                 sequenceOf(lazy { original.map { (it + 10).toByte() }.toByteArray() })
@@ -35,7 +33,7 @@ class ByteArraySpliceTest {
         val original = byteArrayOf(1, 2, 3, 4)
         
         mutagen(Random(), original).map { it.value }.forEach { mutant ->
-            assertTrue("original: $original, mutant: $mutant", mutant.find { it > 10 } != null)
+            assertTrue(mutant.find { it > 10 } != null, "original: $original, mutant: $mutant")
         }
     }
 }
