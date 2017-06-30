1adc8708b7836bae727eb50074563bd9b8c0e4d9
Nat Pryce <sw@natpryce.com>
2017-06-30 08:57:01 +0100

    One test runs in JUnit and QUnit

---
+++ common/src/test/com/natpryce/snodge/MutagenAndTest.kt
@@ -0,0 +1,22 @@
+package com.natpryce.snodge
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+
+class MutagenAndTest {
+    @Test
+    fun for_each_mutant_returns_the_mutant_and_further_mutations_of_the_mutant() {
+        val mutagen = always("alice", "bob")
+            .and { _, mutant -> sequenceOf(lazy { mutant.toUpperCase() }, lazy { mutant.substring(0, 1) }) }
+        
+        val mutants = mutagen(Random(), "original").map { it.value }.toSet()
+        
+        assertEquals(
+            setOf(
+                "alice", "ALICE", "a",
+                "bob", "BOB", "b"
+            ),
+            mutants)
+    }
+}
