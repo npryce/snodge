2b33fba02f6e47e9fd18bdd4040ccd10af6d57a7
Nat Pryce <sw@natpryce.com>
2017-07-02 18:38:55 +0100

    Mutagen<XmlDocument>.forStrings() implemented for JS platform

---
+++ common/src/test/com/natpryce/snodge/xml/XmlMutagenForStringsTest.kt
@@ -0,0 +1,39 @@
+package com.natpryce.snodge.xml;
+
+import com.natpryce.snodge.Random
+import com.natpryce.snodge.mutant
+import com.natpryce.xmlk.toXmlDocument
+import org.junit.Test
+import kotlin.test.assertNotEquals
+
+class XmlMutagenForStringsTest {
+    val mutagen = replaceText("XXXX")
+    val random = Random()
+    
+    @Test
+    fun can_mutate_xml_text() {
+        val original = """
+        |<example>
+        |  <!-- a comment -->
+        |  <a>A</a>
+        |  <b><![CDATA[B]]></b>
+        |  <c:d xmlns:c="http://example.com/c" e="f">G</c:d>
+        |  <h>
+        |    <i>I</i>
+        |    <j>J</j>
+        |  </h>
+        |</example>
+        """.trimMargin()
+        
+        val mutant = random.mutant(mutagen.forStrings(), original)
+        
+        assertNotEquals(actual = mutant, illegal = original)
+        
+        canParse(mutant)
+    }
+    
+    private fun canParse(mutated: String) {
+        // Does not blow up
+        mutated.toXmlDocument()
+    }
+}
--- platform/js/src/main/com/natpryce/xmlk/parse_js.kt
+++ platform/js/src/main/com/natpryce/xmlk/parse_js.kt
@@ -1,5 +1,39 @@
 package com.natpryce.xmlk
 
-fun String.toXmlDocument(): XmlDocument {
-    TODO("not implemented")
+import org.w3c.dom.CDATASection
+import org.w3c.dom.Comment
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.Node
+import org.w3c.dom.NodeList
+import org.w3c.dom.ProcessingInstruction
+import org.w3c.dom.Text
+import org.w3c.dom.get
+import org.w3c.dom.parsing.DOMParser
+
+
+fun String.toXmlDocument(): XmlDocument =
+    DOMParser().parseFromString(this, "application/xml").toXmlDocument()
+
+fun Document.toXmlDocument(): XmlDocument =
+    XmlDocument(null, childNodes.map {node -> node.toXmlNode()})
+
+private fun Node.toXmlNode(): XmlNode =
+    when(this) {
+        is Element -> this.toXmlElement()
+        is Text -> XmlText(wholeText, asCData = false)
+        is CDATASection -> XmlText(wholeText, asCData = true)
+        is ProcessingInstruction -> XmlProcessingInstruction(target, data.takeIf { it.isNotEmpty() })
+        is Comment -> XmlComment(textContent ?: "")
+        else -> throw IllegalArgumentException("cannot convert ${this::class.simpleName} to XmlNode")
+    }
+
+private fun Element.toXmlElement(): XmlElement {
+    return XmlElement(
+        name = QName(localName, namespaceURI, prefix),
+        attributes = emptyMap(), // TODO parse attributes
+        children = childNodes.map { it.toXmlNode() })
 }
+
+private fun NodeList.map(f: (Node)->XmlNode): List<XmlNode> =
+    (0 until length).map { f(get(it)!!) }
