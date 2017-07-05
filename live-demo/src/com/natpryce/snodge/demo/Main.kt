edcaa787509c599265bc787277b2c51464d57a11
Nat Pryce <sw@natpryce.com>
2017-07-05 13:04:55 +0100

    Demo can mutate XML and plain text as well.

--- live-demo/src/com/natpryce/snodge/demo/Main.kt
+++ live-demo/src/com/natpryce/snodge/demo/Main.kt
@@ -1,11 +1,17 @@
 package com.natpryce.snodge.demo
 
+import com.natpryce.snodge.Mutagen
 import com.natpryce.snodge.Random
 import com.natpryce.snodge.json.defaultJsonMutagens
 import com.natpryce.snodge.json.forStrings
 import com.natpryce.snodge.mutant
+import com.natpryce.snodge.text.replaceWithPossiblyMeaningfulText
+import com.natpryce.snodge.text.splice
+import com.natpryce.snodge.xml.defaultXmlMutagens
+import com.natpryce.snodge.xml.forStrings
 import org.w3c.dom.HTMLButtonElement
 import org.w3c.dom.HTMLElement
+import org.w3c.dom.HTMLSelectElement
 import org.w3c.dom.HTMLTextAreaElement
 import kotlin.browser.document
 import kotlin.browser.window
@@ -17,6 +23,7 @@ external class SyntaxError : Throwable
 val random = Random()
 val originalTextArea = document.getElementById("original") as HTMLTextAreaElement
 val mutantTextArea = document.getElementById("mutant") as HTMLTextAreaElement
+val inputTypeSelector = document.getElementById("type") as HTMLSelectElement
 
 fun main(args: Array<String>) {
     val mutateButton = document.getElementById("mutate") as HTMLButtonElement
@@ -40,15 +47,24 @@ fun main(args: Array<String>) {
     }
 }
 
+fun selectedMutagen(): Mutagen<String> = inputTypeSelector.value.let {
+    when(it) {
+        "text" -> splice(replaceWithPossiblyMeaningfulText())
+        "json" -> defaultJsonMutagens().forStrings()
+        "xml" -> defaultXmlMutagens().forStrings()
+        else -> throw IllegalStateException("unrecognised input type: $it")
+    }
+}
+
 private fun mutate() {
     val originalText = originalTextArea.value
     try {
-        val mutantText = random.mutant(defaultJsonMutagens().forStrings(), originalText)
+        val mutantText = random.mutant(selectedMutagen(), originalText)
         mutantTextArea.value = mutantText
         clearError()
     }
     catch (e: SyntaxError) {
-        e.message?.let(::reportError)
+        reportError(e.message ?: "could not parse input")
     }
 }
 
