e3b93604f5fe61987e47685f7e44956f2100660c
Nat Pryce <sw@natpryce.com>
2017-08-12 00:48:57 +0100

    Refactoring

--- common/src/main/com/natpryce/snodge/json/JsonMutagen.kt
+++ common/src/main/com/natpryce/snodge/json/JsonMutagen.kt
@@ -1,14 +1,34 @@
 package com.natpryce.snodge.json
 
+import com.natpryce.jsonk.JsonArray
 import com.natpryce.jsonk.JsonElement
+import com.natpryce.jsonk.JsonObject
+import com.natpryce.jsonk.replace
 import com.natpryce.jsonk.toJsonElement
 import com.natpryce.jsonk.toJsonString
+import com.natpryce.jsonk.withProperty
 import com.natpryce.snodge.Mutagen
 import com.natpryce.snodge.mapped
 import com.natpryce.snodge.recurse
+import com.natpryce.snodge.walk
 
 
 inline fun <reified T : JsonElement> JsonMutagen(crossinline elementMutagen: Mutagen<T>): Mutagen<JsonElement> =
-    recurse(JsonElement::walk, elementMutagen)
+    recurse({ e: JsonElement -> e.walk() }, elementMutagen)
 
 fun Mutagen<JsonElement>.forStrings() = mapped({ it.toJsonElement() }, { it.toJsonString() })
+
+fun JsonElement.walk(): Sequence<Pair<JsonElement, (JsonElement) -> JsonElement>> = walk {
+    when (it) {
+        is JsonObject ->
+            it.asSequence().map { (key, child) ->
+                child to { newChild: JsonElement -> it.withProperty(key to newChild) }
+            }
+        is JsonArray ->
+            it.asSequence().withIndex().map { (i, child) ->
+                child to { newChild: JsonElement -> it.replace(i, newChild) }
+            }
+        else ->
+            emptySequence()
+    }
+}
--- common/src/main/com/natpryce/snodge/xml/XmlMutagen.kt
+++ common/src/main/com/natpryce/snodge/xml/XmlMutagen.kt
@@ -3,13 +3,43 @@ package com.natpryce.snodge.xml
 import com.natpryce.snodge.Mutagen
 import com.natpryce.snodge.mapped
 import com.natpryce.snodge.recurse
+import com.natpryce.snodge.walk
 import com.natpryce.xmlk.XmlDocument
+import com.natpryce.xmlk.XmlElement
 import com.natpryce.xmlk.XmlNode
+import com.natpryce.xmlk.defaultDOMImplementation
+import com.natpryce.xmlk.toDOM
 import com.natpryce.xmlk.toXmlDocument
 import com.natpryce.xmlk.toXmlString
+import org.w3c.dom.DOMImplementation
+import org.w3c.dom.Document
 
 inline fun <reified T : XmlNode> XmlMutagen(crossinline elementMutagen: Mutagen<T>): Mutagen<XmlDocument> =
-    recurse(XmlDocument::walk, elementMutagen)
+    recurse({ d: XmlDocument -> d.walk() }, elementMutagen)
 
-fun Mutagen<XmlDocument>.forStrings() = mapped({it.toXmlDocument()}, {it.toXmlString()})
+fun Mutagen<XmlDocument>.forStrings() = mapped({ it.toXmlDocument() }, { it.toXmlString() })
+
+fun Mutagen<XmlDocument>.forDOM(implementation: DOMImplementation = defaultDOMImplementation()): Mutagen<Document> =
+    mapped({it.toXmlDocument()}, {it.toDOM(implementation)})
+
+
+fun XmlDocument.walk(): Sequence<Pair<XmlNode, (XmlNode) -> XmlDocument>> =
+    children.asSequence().withIndex().flatMap { (i, child) ->
+        child.walk(XmlNode::listChildren, { copy(children = children.replace(i, it)) })
+    }
+
+private fun XmlNode.listChildren(): Sequence<Pair<XmlNode, (XmlNode) -> XmlNode>> =
+    if (this is XmlElement) {
+        children.asSequence().withIndex().map { (i, child) -> child to childReplacer(i) }
+    }
+    else {
+        emptySequence()
+    }
+
+private fun XmlElement.childReplacer(i: Int) =
+    fun(newChild: XmlNode) =
+        copy(children = children.replace(i, newChild))
+
+private fun <T> List<T>.replace(i: Int, newElement: T) =
+    toMutableList().apply { set(i, newElement) }.toList()
 
